d6e6038b4d9d12221f8adf44d9f96a3ddeb8f84d
jeanmmlima <jeanmariomlima@gmail.com>
2021-07-22 10:43:47 -0300

    graphqlok

--- server-graphql.js
+++ server-graphql.js
@@ -1,28 +1,62 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
+const { argsToArgsConfig } = require('graphql/type/definition');
+
+const jogos = [
+  {
+    id: 1,
+    nome: 'FFVII',
+    nivel: 'Dificil'
+  },
+  {
+    id: 2,
+    nome: 'FFIX',
+    nivel: 'Medio'
+  }
+]
  
 var schema = buildSchema(`
   type Query {
-    hello: String
+    idade: Int
+    salario: Float
     nome: String
-    idade: String
-    heroi: Hero
+    ativo:Boolean
+    id: ID
+    endereco: Address
+    jogos: [Jogo]
   }
-  type Hero {
-      nome: String
+  type Address {
+      rua: String
+      numero: Int
+  }
+  type Jogo {
+    nome: String
+    nivel: String
   }
 `);
 
-
+const { endereco } = {rua: 'leonel',numero:14};
  
-var root = {hello: () => 'Hello world!',
-            nome:() => 'Jean' };
+var resolvers = {
+  
+  idade: () => 28,
+  salario: () => 1000.54,
+  nome:() => 'Jean',
+  ativo:() => true,
+  endereco:() => {
+    return {rua: 'teste',numero:13};
+  },
+  jogos:() => {
+    return jogos;
+  }
+
+};
  
 var app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
+  rootValue: resolvers,
   graphiql: true,
 }));
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
